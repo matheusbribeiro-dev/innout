5f54030417c1211723b71ca68f866948e1c84f87
Matheus Ribeiro <68439491+matheusbribeiro-dev@users.noreply.github.com>
2020-11-18 09:03:35 -0300

    Class 248: front end login

--- public/index.php
+++ public/index.php
@@ -1,11 +1,4 @@
 <?php
 /*https://www.php.net/manual/pt_BR/function.dirname*/
 require_once(dirname(__FILE__, 2) . '/src/config/config.php');
-require_once(dirname(__FILE__, 2) . '/src/models/User.php');
-
-$user = new User([]);
-print_r(User::get(['id' => 1], 'name, email'));
-
-// echo $user->getSelect(['id' => 1], 'name, email');
-// echo '<br>';
-// echo $user->getSelect(['name' => 'Chaves', 'email' => 'chaves@cod3r.com.br']);
+require_once(dirname(__FILE__, 2) . '/src/views/login.php');
---
+++ src/views/login.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSS -->
+    <link rel="stylesheet" href="assets/css/comum.css">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/icofont.min.css">
+    <link rel="stylesheet" href="assets/css/login.css">
+
+    <!-- title -->
+    <title>In N' Out</title>
+</head>
+<body>
+    <form class="form-login" action="#" method="post">
+        <div class="login-card card">
+            <div class="card-header">
+                <i class="icofont-travelling mr-2"></i>
+                <span class="font-weight-light">In </span>
+                <span class="font-weight-bold mx-1">N'</span>
+                <span class="font-weight-light">Out</span>
+                <i class="icofont-runner-alt-1 ml-2"></i>
+            </div> 
+            <div class="card-body">
+                <!-- from email -->
+                <div class="form-group">   
+                    <label for="email">E-mail</label>
+                    <input type="email" id="email" name="email"
+                        class="form-control"
+                        placeholder="Informe o e-mail" autofocus>
+                </div>
+                <!-- / from email -->
+
+                <!-- from senha -->
+                <div class="form-group">  
+                    <label for="password">Senha</label>
+                    <input type="password" id="password" name="password"
+                        class="form-control"
+                        placeholder="Informe a senha">
+                </div>
+                <!-- / from senha -->
+            </div> 
+            <div class="card-footer">
+                <button class="btn btn-lg btn-primary ml-auto">Entrar</button>
+            </div>
+        </div>
+    </form>
+</body> 
+</html>
